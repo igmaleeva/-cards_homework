d47424d298d9341490094d60a72685e828c58f79
malina <malina021082@mail.ru>
2022-11-30 10:16:42 +0100

    Unicode suit symbols

--- cards_homework.rb
+++ cards_homework.rb
@@ -1,6 +1,20 @@
-# Массивы с наборами мастей и достоинств
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+# Масти — юникод-символы.
+#
+# (с) goodprogrammer.ru
 
-# Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# Создадим массив с набором мастей, вместо названия масти — иконка-символ
+suits = ["♦", "♠", "♥ ", "♣"]
+
+puts "#{values.sample}#{suits.sample}"
